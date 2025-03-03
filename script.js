3d25ff49266b8d524e00dda0195566d915865fed
Ahad-Awan <muhammadahad203813@gmail.com>
2025-03-03 14:17:07 +0500

    add styles

---
+++ script.js
@@ -0,0 +1,16 @@
+let addCart = document.querySelectorAll(".addCart");
+// let viewCart = document.querySelectorAll("viewCart");
+
+let arr = [];
+
+addCart.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("btn click");
+  });
+});
+
+// function addCart() {
+//   console.log("btn click");
+// }
+
+// addBtn.addEventListener("click", addCart);
